c53039eac9f7cffab15b00d063edc19a02ad1f3b
cliffyan <cliffyan@qq.com>
2017-07-16 02:52:55 +0800

    del all

--- readme.md
+++
@@ -1,7 +0,0 @@
-run:
---
-mvn spring-boot:run
---
-nvm install
-cd target
-java -jar springboot_demo-0.0.1-SNAPSHOT.jar
--- src/main/java/com/example/demo/utils/Constants.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.utils;
-
-/**
- * Created by cliffyan on 2017/7/16.
- */
-public class Constants {
-    public static final String STR_SUCCESS = "suc";
-    public static final String STR_FAIL = "fail";
-}

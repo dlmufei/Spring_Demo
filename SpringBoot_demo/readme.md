57b2ac2884d3e6d0c0c85ad8874206aa560fdea5
cliffyan <cliffyan@qq.com>
2017-07-16 12:52:57 +0800

    jar包改为war包部署改造

--- SpringBoot_demo/readme.md
+++ SpringBoot_demo/readme.md
@@ -1,4 +1,4 @@
-## 运行方法
+## jar包打包及运行方法
 ### 方法1:
 > mvn spring-boot:run
 ### 方法2:
@@ -8,5 +8,16 @@
 
 > java -jar springboot_demo-0.0.1-SNAPSHOT.jar
 
-## 访问接口
+### 访问接口
 > http://localhost:8080/demo/index
+
+## jar包改为war包
+### tomcat部署包
+> 拷贝到tomcat webapp目录: cp target/springbootdemo.war ~/dev/tomcat/apache-tomcat-7.0.70/webapps
+
+> 启动tomcat: ~/dev/tomcat/apache-tomcat-7.0.70/bin/startup.sh
+
+### 访问链接
+> http://localhost:8080/springbootdemo/demo/index
+
+### 详细改动见pom.xml文件修改,git提交log
---
+++ SpringBoot_demo/src/main/java/com/qq/springbootdemo/SpringBootStartApplication.java
@@ -0,0 +1,17 @@
+package com.qq.springbootdemo;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+/**
+ * 修改启动类，继承 SpringBootServletInitializer 并重写 configure 方法
+ * <p>
+ * Created by cliffyan on 2017/7/16.
+ */
+public class SpringBootStartApplication extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        // 注意这里要指向原先用main方法执行的Application启动类
+        return builder.sources(Application.class);
+    }
+}
